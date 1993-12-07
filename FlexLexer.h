cd0e94e833e69b7b52210125b74f2728bdbb2afb
Vern Paxson <vern@ee.lbl.gov>
1993-12-07 10:11:41 +0000

    Support for yyFlexLexer::LexerError

--- FlexLexer.h
+++ FlexLexer.h
@@ -104,6 +104,7 @@ public:
 protected:
 	virtual int LexerInput( char* buf, int max_size );
 	virtual void LexerOutput( const char* buf, int size );
+	virtual void LexerError( const char* msg );
 
 	void yyunput( int c, char* buf_ptr );
 	int yyinput();
