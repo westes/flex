798e5a60fc028f87b4e67cc49ce15fad6c8e0a83
Will Estes <wlestes@users.sourceforge.net>
2002-10-07 15:29:46 +0000

    ok, here goes; try to handle integral typedefs in one swell foop

--- flexdef.h
+++ flexdef.h
@@ -48,73 +48,10 @@
 #ifdef HAVE_LIMITS_H
 #include <limits.h>
 #endif
-#ifdef HAVE_STDINT_H
-#include <stdint.h>
-#endif
-#ifndef HAVE_INTTYPES_H_WITH_UINTMAX
-/* Exact integral types.  */
-
-/* Signed.  */
-
-#ifndef __int8_t_defined
-# define __int8_t_defined
-typedef signed char int8_t;
-typedef short int int16_t;
-typedef int int32_t;
-
-# if __WORDSIZE == 64
-typedef long int int64_t;
-# else
-__extension__ typedef long long int int64_t;
-# endif
-#endif
-
-/* Unsigned.  */
-typedef unsigned char uint8_t;
-typedef unsigned short int uint16_t;
-typedef unsigned int uint32_t;
-
-#if __WORDSIZE == 64
-typedef unsigned long int uint64_t;
-#else
-__extension__ typedef unsigned long long int uint64_t;
-#endif
-#endif /* ! HAVE_INTTYPES_H_WITH_UINTMAX */
-
-
-/* Limits of integral types. */
-#ifndef INT8_MIN
-#define INT8_MIN               (-128)
-#endif
-#ifndef INT16_MIN
-#define INT16_MIN              (-32767-1)
-#endif
-#ifndef INT32_MIN
-#define INT32_MIN              (-2147483647-1)
-#endif
-#ifndef INT8_MAX
-#define INT8_MAX               (127)
-#endif
-#ifndef INT16_MAX
-#define INT16_MAX              (32767)
-#endif
-#ifndef INT32_MAX
-#define INT32_MAX              (2147483647)
-#endif
-#ifndef UINT8_MAX
-#define UINT8_MAX              (255U)
-#endif
-#ifndef UINT16_MAX
-#define UINT16_MAX             (65535U)
-#endif
-#ifndef UINT32_MAX
-#define UINT32_MAX             (4294967295U)
-#endif
-
-
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
 #endif
+
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
