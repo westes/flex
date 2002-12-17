fd1d6538b368cbea75a1c625e2928a843dadf511
John Millaway <john43@users.sourceforge.net>
2002-12-17 23:53:06 +0000

    Fixed bug submitted by Bojan Smojver <bojan@rexursive.com> where the use of yylineno, reentrant, and yymore together caused a compile-time error.

--- gen.c
+++ gen.c
@@ -1985,7 +1985,7 @@ void make_tables ()
 	indent_puts ("{");
 	indent_puts ("int yyl;");
 	do_indent ();
-	out_str ("for ( yyl = %s; yyl < yyleng; ++yyl )\n",
+	out_str ("for ( yyl = YY_G(%s); yyl < yyleng; ++yyl )\n",
 		 yymore_used ? (yytext_is_array ? "yy_prev_more_offset" :
 				"yy_more_len") : "0");
 	indent_up ();
