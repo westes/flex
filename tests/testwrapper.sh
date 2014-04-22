382be5cdc571d080daa540c562cd11e4e9ca1434
Will Estes <westes575@gmail.com>
2014-04-22 18:38:15 -0400

    refactor basic-nr test for new test suite layout

---
+++ tests/testwrapper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# testwrapper.sh: run a flex test, typically called by a Makefile
+
+# Each test will exercise some feature or aspect of flex. Run the test with any input it may need.
+
+INPUTFILE=/dev/null
+
+TESTNAME="$1"
+
+
