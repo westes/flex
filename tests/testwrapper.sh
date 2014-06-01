fddc982e9f447f22248397f9e94e76c93e1d723f
Will Estes <westes575@gmail.com>
2014-06-01 15:09:29 -0400

    make tests/testwrapper.sh more verbose; find input in srcdir
    Since output is redirected by the automake parallel test suite driver,
    turn on both -v and -x in bash for the testwrapper.sh shell
    script. This helps a ton in debugging problems with the test harness
    itself.
    
    In general, the input files are in automake's srcdir and the name of
    the test includes the relative path to it (even though that's supposed
    to be ./). Therefore, pass srcdir in AM_LOG_FLAGS and prepend that to
    the test name as part of constructing the input file's name.

--- tests/testwrapper.sh
+++ tests/testwrapper.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash -vx
 
 # testwrapper.sh: run a flex test, typically called by a Makefile
 
 # Each test will exercise some feature or aspect of flex. Run the test with any input it may need.
 
-INPUTFILE=/dev/null
+SRCDIR="$1"
+TESTNAME="$2"
+INPUTNAME=`basename $TESTNAME`.txt
 
-TESTNAME="$1"
-
-
+$TESTNAME < $SRCDIR/$INPUTNAME
