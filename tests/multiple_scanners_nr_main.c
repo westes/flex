323c11e8aef18a09aa40d11a4fabf6b8c787d782
Will Estes <westes575@gmail.com>
2014-06-15 19:23:53 -0400

    refactor multiple_scanners_nr test for new test suite layout

--- tests/multiple_scanners_nr_main.c
+++ tests/multiple_scanners_nr_main.c
@@ -21,30 +21,31 @@
  * PURPOSE.
  */
 
-%{
-/* A template scanner file to build "scanner-1.c" and "scanner-1.h". */
-#include <stdio.h>
-#include <stdlib.h>
-#include "config.h"
+#include "multiple_scanners_nr_1.h"
+#include "multiple_scanners_nr_2.h"
 
-%}
+int
+main ( int argc, char** argv )
+{
+    int S1_ok=1, S2_ok=1;
+    YY_BUFFER_STATE buff1, buff2;
+    S1_out = S2_out = stdout;
+    buff1 = S1__scan_string("foo on bar off");
+    buff2 = S2__scan_string("on blah blah off foo on bar off");
 
-%option header="scanner-1.h"
-%option 8bit outfile="scanner-1.c" prefix="S1_"
-%option nounput nomain noyywrap
-%option warn stack noyy_top_state
-
-%x ON
-%x OFF
-%%
-<INITIAL>{
-on    yy_push_state(ON); return 10;
-off   yy_push_state(OFF); return 11;
-.|\n  return 12;
+    /* scan simultaneously. */
+    while(S1_ok || S2_ok)
+    {
+        if (S1_ok)
+            S1_ok = S1_lex();
+        if (S2_ok)
+            S2_ok = S2_lex();
+    }
+    S1__delete_buffer(buff1);
+    S2__delete_buffer(buff2);
+    printf("TEST RETURNING OK.\n");
+    return 0;
 }
-<ON>.|\n  yy_pop_state(); return 13;
 
-<OFF>.|\n yy_pop_state(); return 14;
 
-%%
-
+/* vim:set tabstop=8 softtabstop=4 shiftwidth=4: */
