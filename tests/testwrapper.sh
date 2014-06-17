75633654139bdd6ebe4569cd976698fd78469f78
Will Estes <westes575@gmail.com>
2014-06-16 20:07:56 -0400

    refactor rescan_r test for new test suite layout

--- tests/testwrapper.sh
+++ tests/testwrapper.sh
@@ -6,22 +6,27 @@
 
 INPUT_DIRECTORY=""
 INPUT_NAME=""
+USE_REDIRECT=0
 
 while getopts :d:i:rt OPTION ; do
     case $OPTION in
         d) INPUT_DIRECTORY=$OPTARG ;;
-        i) INPUTNAME="$OPTARG" ;;
+        i) INPUT_NAME="$OPTARG" ;;
         r) USE_REDIRECT=1 ;;
         t) USE_TABLES=1 ;;
     esac
     done
 
-TESTNAME="${!$OPTIND}"
+TESTNAME="${!OPTIND}"
 
-INPUTNAME=${INPUT_NAME:-`basename $TESTNAME`.txt}
+INPUT_NAME=${INPUT_NAME:-`basename $TESTNAME`.txt}
 
-if [ -f $INPUT_DIRECTORY/$INPUT_NAME ] ; then
-    $TESTNAME ${USE_TABLES:+${INPUT_DIRECTORY}${TESTNAME}} ${USE_REDIRECT:+'<'} $INPUT_DIRECTORY/$INPUT_NAME
+if [ -f ${INPUT_DIRECTORY}/${INPUT_NAME} ] ; then
+    if [ $USE_REDIRECT == 1 ] ; then
+        $TESTNAME ${USE_TABLES:+${INPUT_DIRECTORY}/${TESTNAME}.tables} < $INPUT_DIRECTORY/$INPUT_NAME
+    else
+        $TESTNAME ${USE_TABLES:+${INPUT_DIRECTORY}/${TESTNAME}.tables} $INPUT_DIRECTORY/$INPUT_NAME
+    fi
 else
     $TESTNAME
 fi
