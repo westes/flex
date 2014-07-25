2d257614dd66f410375f24ce1f2306b3c260bd29
Mariusz Pluciński <mplucinski@mplucinski.com>
2014-07-25 15:22:01 +0200

    Allow error reporting routines to accept varying number of arguments in modern style

--- src/scanflags.c
+++ src/scanflags.c
@@ -61,8 +61,7 @@ sf_init (void)
     assert(_sf_stk == NULL);
     _sf_stk = (scanflags_t*) flex_alloc ( sizeof(scanflags_t) * (_sf_max = 32));
     if (!_sf_stk)
-        lerrsf_fatal(_("Unable to allocate %ld of stack"),
-            (long)sizeof(scanflags_t));
+        lerrsf_fatal(_("Unable to allocate %ld of stack"), sizeof(scanflags_t));
     _sf_stk[_sf_top_ix] = 0;
 }
 
