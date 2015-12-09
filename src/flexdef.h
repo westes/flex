1f62804772d84e29843c171a392f4df61c7efe3c
Michael McConville <mmcconville@mykolab.com>
2015-12-09 14:20:40 -0500

    Remove alloca() configuration
    alloca() is deprecated and dangerous. Unless I'm missing something, we
    should never be using it.

--- src/flexdef.h
+++ src/flexdef.h
@@ -39,21 +39,6 @@
 #include <config.h>
 #endif
 
-/* AIX requires this to be the first thing in the file.  */
-#if !defined(__GNUC__) && !defined(__lint__)
-# if HAVE_ALLOCA_H
-#  include <alloca.h>
-# else
-#  ifdef _AIX
- #pragma alloca
-#  else
-#   ifndef alloca /* predefined by HP cc +Olibcalls */
-char *alloca ();
-#   endif
-#  endif
-# endif
-#endif
-
 #ifdef STDC_HEADERS
 #include <stdio.h>
 #include <stdlib.h>
