713f5a0cb4ba5e4bfebae125edf4111fd8dae803
Explorer09 <explorer09@gmail.com>
2016-11-19 02:37:04 +0800

    build: Link $(LIBOBJS) from src/ dir, remove libcompat.la.
    The libcompat.la library was small and less friendly to bootstrapping
    and cross compilation. Now, we will simply link individual object
    files as needed, which is simpler.

--- lib/lib.c
+++
@@ -1,9 +0,0 @@
-/* Since building an empty library could cause problems, we provide a
- * function to go into the library. We could make this non-trivial by
- * moving something that flex treats as a library function into this
- * directory. */
-
-extern void do_nothing(void);
-
-void do_nothing(void){ return;}
-
