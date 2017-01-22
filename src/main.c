752c6b79b14221e1dbdf08c468e3f31f6fe8e907
Christos Zoulas <christos@zoulas.com>
2017-01-22 18:20:44 +0100

    Use the prefix when defining yywrap to avoid redefinitions.

--- src/main.c
+++ src/main.c
@@ -1586,9 +1586,9 @@ void readin (void)
 	if (!do_yywrap) {
 		if (!C_plus_plus) {
 			 if (reentrant)
-				outn ("\n#define yywrap(yyscanner) (/*CONSTCOND*/1)");
+				out_str ("\n#define %swrap(yyscanner) (/*CONSTCOND*/1)\n", prefix);
 			 else
-				outn ("\n#define yywrap() (/*CONSTCOND*/1)");
+				out_str ("\n#define %swrap() (/*CONSTCOND*/1)\n", prefix);
 		}
 		outn ("#define YY_SKIP_YYWRAP");
 	}
