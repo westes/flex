cb525e7306a869a7b6ae0199968f5446fd6e4f33
Kang-Che Sung <explorer09@gmail.com>
2018-02-23 11:07:29 +0800

    travis: Hard-code hashes for verifying tarballs
    GPG signatures require an external keyserver which might be offline,
    which is undesirable for build server use. It's equally secure to just
    hard-code the hashes, provided they're trusted (i.e. you verify a hash
    against a GPG signature once).
    
    Fixes: #311.
    
    As a side note: the original two signatures
    (gettext-0.19.8.1.tar.lz.sig and automake-1.15.1.tar.gz.sig) are signed
    against the files' SHA-1 hash.

--- .travis/install-automake.sh
+++ .travis/install-automake.sh
@@ -1,8 +1,18 @@
 #!/bin/bash -ex
 
-wget -nv https://ftp.gnu.org/gnu/automake/automake-1.15.1.tar.gz{,.sig}
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 94604D37
-gpg2 automake-1.15.1.tar.gz.sig 
+wget -nv https://ftp.gnu.org/gnu/automake/automake-1.15.1.tar.gz
+
+# Verify tarball against hard-coded hashes. GPG signatures require an external
+# keyserver which might be offline, which is undesirable for build server use.
+# It's equally secure to just hard-code hashes, provided they're trusted (i.e.
+# you verify a hash against a GPG signature once).
+echo 'd3cd5fc9bbea9f977b51799180cde5d253dcba96 *automake-1.15.1.tar.gz'|
+    sha1sum -c || :
+printf '%s *automake-1.15.1.tar.gz\n' \
+f0d4717ebe2c76cec5d487de090f6e1c0f784b0d382fd964ffa846287e2a364a\
+52531a26ab98b7033ac04ed302a247b3b114299def54819a03439bfc962ff61b|
+    sha512sum -c || :
+
 tar xf automake-1.15.1.tar.gz
 cd automake-1.15.1
 ./configure --prefix=$HOME
