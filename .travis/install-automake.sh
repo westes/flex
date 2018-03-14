26da44d2f401c6f44e9c1346297817a63c6f9c2f
Explorer09 <explorer09@gmail.com>
2018-03-14 21:55:24 +0800

    travis: Use Automake 1.16.1.

--- .travis/install-automake.sh
+++ .travis/install-automake.sh
@@ -1,20 +1,21 @@
 #!/bin/bash -ex
 
-wget -nv https://ftp.gnu.org/gnu/automake/automake-1.15.1.tar.gz
+wget -nv https://ftp.gnu.org/gnu/automake/automake-1.16.1.tar.gz
 
 # Verify tarball against hard-coded hashes. GPG signatures require an external
 # keyserver which might be offline, which is undesirable for build server use.
 # It's equally secure to just hard-code hashes, provided they're trusted (i.e.
 # you verify a hash against a GPG signature once).
-echo 'd3cd5fc9bbea9f977b51799180cde5d253dcba96 *automake-1.15.1.tar.gz'|
-    sha1sum -c || :
-printf '%s *automake-1.15.1.tar.gz\n' \
-f0d4717ebe2c76cec5d487de090f6e1c0f784b0d382fd964ffa846287e2a364a\
-52531a26ab98b7033ac04ed302a247b3b114299def54819a03439bfc962ff61b|
+printf '%s *automake-1.16.1.tar.gz\n' \
+608a97523f97db32f1f5d5615c98ca69326ced2054c9f82e65bade7fc4c9dea8|
+    sha256sum -c || :
+printf '%s *automake-1.16.1.tar.gz\n' \
+47b0120a59e3e020529a6ce750297d7de1156fd2be38db5d101e50120f11b40c\
+28741ecd5eacf2790a9e25386713dcf7717339cfa5d7943d0dbf47c417383448|
     sha512sum -c || :
 
-tar xf automake-1.15.1.tar.gz
-cd automake-1.15.1
+tar xf automake-1.16.1.tar.gz
+cd automake-1.16.1
 # Don't flood Travis CI build log with dependency packages unless error occurs.
 ./configure --quiet --prefix="$HOME" ||
     { s=$? && cat config.log && (exit $s); }
