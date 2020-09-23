0f2a58a818598559f150a7d9d42a7d8ba0b3bc16
Eric S. Raymond <esr@thyrsus.com>
2020-09-22 22:54:43 -0400

    Partial m4ization of nultrans code.
    Also, remove two insertion points in the skel file
    that are no longer used because we're now using m4
    conditionals instead.
    
    In the generated C, two magic comments from the insertion points go
    away.
    
    #27 in the retargeting patch series

--- src/gen.c
+++ src/gen.c
@@ -1361,8 +1361,6 @@ void make_tables (void)
 	skelout ();		/* %% [14.0] - break point in skel */
 	skelout ();		/* %% [15.0] - break point in skel */
 	skelout ();		/* %% [16.0] - break point in skel */
-
-	set_indent (1);
 	skelout ();		/* %% [17.0] - break point in skel */
 
 	/* Generate the code to make a NUL transition. */
@@ -1370,16 +1368,11 @@ void make_tables (void)
 	/* Only generate a definition for "yy_cp" if we'll generate code
 	 * that uses it.  Otherwise lint and the like complain.
 	 */
-	int     need_backing_up = (num_backing_up > 0 && !reject);
-
-	if (need_backing_up && (!nultrans || fullspd || fulltbl))
-		/* We're going to need yy_cp lying around for the call
-		 * below to gen_backing_up().
-		 */
-		indent_puts ("char *yy_cp = YY_G(yy_c_buf_p);");
+	indent_puts ("m4_ifdef([[M4_MODE_NEED_YY_CP]], [[	char *yy_cp = YY_G(yy_c_buf_p);]])");
 
 	outc ('\n');
 
+	set_indent (1);
 	if (nultrans) {
 		indent_puts
 			("yy_current_state = yy_NUL_trans[yy_current_state];");
@@ -1388,10 +1381,8 @@ void make_tables (void)
 
 	else if (fulltbl) {
 		do_indent ();
-		if (gentables)
-			outn ("yy_current_state = yy_nxt[yy_current_state][YY_NUL_EC];");
-		else
-			outn ("yy_current_state = yy_nxt[yy_current_state*YY_NXT_LOLEN + YY_NUL_EC];");
+		outn ("m4_ifdef([[M4_MODE_GENTABLES]], [[yy_current_state = yy_nxt[yy_current_state][YY_NUL_EC];]])");
+		outn ("m4_ifdef([[M4_MODE_NO_GENTABLES]], [[yy_current_state = yy_nxt[yy_current_state*YY_NXT_LOLEN + YY_NUL_EC];]])");
 		indent_puts ("yy_is_jam = (yy_current_state <= 0);");
 	}
 
@@ -1410,30 +1401,28 @@ void make_tables (void)
 	}
 
 	else {
-		char    NUL_ec_str[20];
-
 		out ("M4_GEN_NEXT_COMPRESSED_STATE(YY_NUL_EC)");
 
 		indent_puts ("yy_is_jam = (yy_current_state == YY_JAMSTATE);");
 
-		if (reject) {
-			/* Only stack this state if it's a transition we
-			 * actually make.  If we stack it on a jam, then
-			 * the state stack and yy_c_buf_p get out of sync.
-			 */
-			indent_puts ("if ( ! yy_is_jam )");
-			++indent_level;
-			indent_puts
-				("*YY_G(yy_state_ptr)++ = yy_current_state;");
-			--indent_level;
-		}
+		indent_puts ("m4_ifdef([[M4_MODE_USES_REJECT]], [[");
+		/* Only stack this state if it's a transition we
+		 * actually make.  If we stack it on a jam, then
+		 * the state stack and yy_c_buf_p get out of sync.
+		 */
+		indent_puts ("if ( ! yy_is_jam )");
+		++indent_level;
+		indent_puts
+	           ("*YY_G(yy_state_ptr)++ = yy_current_state;");
+		--indent_level;
+		indent_puts ("]])");
 	}
 
 	/* If we've entered an accepting state, back up; note that
 	 * compressed tables have *already* done such backing up, so
 	 * we needn't bother with it again.
 	 */
-	if (need_backing_up && (fullspd || fulltbl)) {
+	indent_puts ("m4_ifdef([[M4_MODE_NULTRANS_WRAP]], [[");
 		outc ('\n');
 		indent_puts ("if ( ! yy_is_jam )");
 		++indent_level;
@@ -1441,10 +1430,8 @@ void make_tables (void)
 		outn ("M4_GEN_BACKING_UP");
 		indent_puts ("}");
 		--indent_level;
-	}
+	indent_puts("]])");
 
-	skelout ();		/* %% [18.0] - break point in skel */
-	skelout ();		/* %% [19.0] - break point in skel */
 	skelout ();
 
 	/* Copy remainder of input to output. */
