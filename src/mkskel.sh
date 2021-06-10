westes/flex

commit 3f2b9a4d630b702f6dd8592014f89d40a6a4bcc1

mkskel.sh: fix EOL issue for CRLF systems

src/mkskel.sh
@@ -48,7 +48,7 @@ sed '/^%#/d
 s/m4_/m4preproc_/g
 s/a4_/4_/g
 s/[\\"]/\\&/g
-s/.*/  "&",/'
+s/[^\r]*/  "&",/'
 
 echo '  0
 };'

commit b171793b2601783184892bcb98380039da63a409

build: Fix unportable sed expression in mkskel.sh
'\r' in sed is not portable, at least to BSD sed (and macOS).
Replace it with embedded CR character in sed expression.

Fixes #294 (regression caused by #243)

Signed-off-by: Kang-Che Sung <explorer09@gmail.com>

src/mkskel.sh
@@ -32,6 +32,10 @@ VERSION=$4
 case $VERSION in
    *[!0-9.]*) echo 'Invalid version number' >&2; exit 1;;
 esac
+
+# '\r' in sed is not portable (e.g. to BSD sed)
+cr=`printf '\015'`
+
 IFS=.
 # we do want word splitting, so we won't put double quotes around it (see IFS above)
 # shellcheck disable=2086
@@ -46,4 +50,5 @@ sed '/^%#/d
 s/m4_/m4preproc_/g
 s/a4_/4_/g
 s/[\\"]/\\&/g
-s/[^\r]*/  "&",/'
+s/'"$cr"'$//
+s/.*/  "&",/'